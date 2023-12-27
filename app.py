b94e0fdc60fa82f1eeb3c5b4e5b06c8ce146c07b
Rehman Khan <rehmankhan03444@gmail.com>
2023-12-27 11:42:23 +0500

    final commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hey man hello!'
+    return 'hey man hello new!'
 
 if __name__ == '__main__':
     app.run(port=80, host='0.0.0.0')
